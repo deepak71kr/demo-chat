1a61ded7f17aa0afdd404dc97506fcc95f6f252b
deepak <deepak.71kr@gmail.com>
2025-02-22 10:53:36 +0530

    done

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -37,7 +37,7 @@ const SignUp = () => {
 						</label>
 						<input
 							type='text'
-							placeholder='John Doe'
+							placeholder='username'
 							className='w-full input input-bordered  h-10'
 							value={inputs.fullName}
 							onChange={(e) => setInputs({ ...inputs, fullName: e.target.value })}
@@ -50,7 +50,7 @@ const SignUp = () => {
 						</label>
 						<input
 							type='text'
-							placeholder='johndoe'
+							placeholder='username'
 							className='w-full input input-bordered h-10'
 							value={inputs.username}
 							onChange={(e) => setInputs({ ...inputs, username: e.target.value })}
@@ -121,14 +121,14 @@ export default SignUp;
 // 						<label className='label p-2'>
 // 							<span className='text-base label-text'>Full Name</span>
 // 						</label>
-// 						<input type='text' placeholder='John Doe' className='w-full input input-bordered  h-10' />
+// 						<input type='text' placeholder='username' className='w-full input input-bordered  h-10' />
 // 					</div>
 
 // 					<div>
 // 						<label className='label p-2 '>
 // 							<span className='text-base label-text'>Username</span>
 // 						</label>
-// 						<input type='text' placeholder='johndoe' className='w-full input input-bordered h-10' />
+// 						<input type='text' placeholder='username' className='w-full input input-bordered h-10' />
 // 					</div>
 
 // 					<div>
